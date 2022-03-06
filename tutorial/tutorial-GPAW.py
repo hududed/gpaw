7de1bcc70771b58cad042fbe43d6d00e178df7c6
Hud Wahab <hudwahab@gmail.com>
2022-03-06 15:34:06 -0700

    4-cores

--- tutorial/tutorial-GPAW.py
+++ tutorial/tutorial-GPAW.py
@@ -1,27 +1,32 @@
 from ase.build import molecule
 from gpaw import GPAW
+from ase.parallel import paropen
 
 a = 8.0
 h = 0.2
 
 energies = {}
-with open(f'results-{h:.2f}.txt', 'w') as resultfile:
 
-    for name in ['H2O', 'H', 'O']:
-        system = molecule(name)
-        system.set_cell((a, a, a))
-        system.center()
+resultfile = paropen('results-%.2f.txt' % h, 'w')
+# with open(f'results-{h:.2f}.txt', 'w') as resultfile:
+
+for name in ['H2O', 'H', 'O']:
+    system = molecule(name)
+    system.set_cell((a, a, a))
+    system.center()
+
+    calc = GPAW(h=h,
+                txt=f'gpaw-{name}-{h:.2f}.txt')
+    if name == 'H' or name == 'O':
+        calc.set(hund=True)
+
+    system.calc = calc
+
+    energy = system.get_potential_energy()
+    energies[name] = energy
+    print(name, energy, file=resultfile)
+
+e_atomization = energies['H2O'] - 2 * energies['H'] - energies['O']
+print(e_atomization, file=resultfile)
+
     
-        calc = GPAW(h=h,
-                    txt=f'gpaw-{name}-{h:.2f}.txt')
-        if name == 'H' or name == 'O':
-            calc.set(hund=True)
-    
-        system.calc = calc
-    
-        energy = system.get_potential_energy()
-        energies[name] = energy
-        print(name, energy, file=resultfile)
-    
-    e_atomization = energies['H2O'] - 2 * energies['H'] - energies['O']
-    print(e_atomization, file=resultfile)
